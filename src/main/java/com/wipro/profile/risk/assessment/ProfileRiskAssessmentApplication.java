fc25be3c494374b66f2e9440b739f2b590cc442f
sudhsi <sudhansu.singh@wipro.com>
2019-02-19 21:02:51 +0530

    initial commit

---
+++ src/main/java/com/wipro/profile/risk/assessment/ProfileRiskAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.profile.risk.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileRiskAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileRiskAssessmentApplication.class, args);
+	}
+
+}
