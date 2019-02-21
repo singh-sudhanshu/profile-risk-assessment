4225cf8251304cbd337ef8dceddb9bef01a36eb3
HA270519 <hariharasubramaniyam.selvam@wipro.com>
2019-02-21 13:41:16 +0530

    login

--- src/main/java/com/wipro/profile/risk/assessment/config/SecurityConfig.java
+++ src/main/java/com/wipro/profile/risk/assessment/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.wipro.profile.risk.assessment.service.UserService;
@@ -26,31 +28,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	 @Autowired
 	 private DataSource dataSource;
 	 
-	 private final String USER_QUERY = "select email, password from user where email = ?";
+	 private final String USER_QUERY = "select email, password from user where email=?";
 	 
-	 private final String ROLE_QUERY = "select u.email, r.name from user user u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id = r.role_id)"
-	 		+ "   where u.email=?";
+	 private final String ROLE_QUERY = "select u.email, r.name from user u inner join users_roles ur on (u.id = ur.user_id) inner join role r on (ur.role_id = r.id) where u.email=sudhansu.singh@wipro.com";
 
 	    @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http
-	        .csrf().disable()
 	                .authorizeRequests()
-	                    .antMatchers(
-	                            "/registration"
-	                            ).permitAll()
-	                    .anyRequest().authenticated()
+	                .antMatchers("/").permitAll()
+	                .antMatchers("/login").permitAll()
+	                .antMatchers("/registration").permitAll()
+	                .antMatchers("/home/**").hasAuthority("ROLE_ADMIN")
+	                .anyRequest()
+	                .authenticated()
 	                .and()
 	                    .formLogin()
-	                        .loginPage("/login")
-	                            .permitAll()
+	                        .loginPage("/login").failureUrl("/login?error=true")
+	                        .defaultSuccessUrl("/home")
+	                        .usernameParameter("email")
+	                        .passwordParameter("password")	                            
 	                .and()
 	                    .logout()
-	                        .invalidateHttpSession(true)
-	                        .clearAuthentication(true)
 	                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-	                        .logoutSuccessUrl("/login?logout")
-	                .permitAll();
+	                        .logoutSuccessUrl("/")
+	                        .and()
+	                        .rememberMe()
+	                        .tokenRepository(tokenRepository())
+	                        .tokenValiditySeconds(60*60)
+	                        .and().exceptionHandling().accessDeniedPage("/access_denied");
+//	                        .invalidateHttpSession(true)
+//	                        .clearAuthentication(true)	                        
+
+
+	    }
+	    
+	    public PersistentTokenRepository tokenRepository() {
+	    	JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+	    	db.setDataSource(dataSource);
+	    	return db;
+	    	
 	    }
 
 	    @Bean
--- src/main/java/com/wipro/profile/risk/assessment/controller/LoginController.java
+++ src/main/java/com/wipro/profile/risk/assessment/controller/LoginController.java
@@ -4,12 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wipro.profile.risk.assessment.model.User;
 
 @Controller
 public class LoginController {
 	
-	@GetMapping("/login")
-    public String login(Model model) {
-        return "login";
-    }	
+	@RequestMapping(value = {"/", "login"}, method = RequestMethod.GET)
+	public ModelAndView login() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("login");
+		return model;
+	}
+	
+	/*@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public ModelAndView registration() {
+		ModelAndView model = new ModelAndView();
+		
+		User user = new User();
+		model.addObject("user", user);
+		model.setViewName("registration");
+		return model;
+	} */
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public ModelAndView home() {
+		
+		ModelAndView model = new ModelAndView();
+		
+		
+	}
+	
+	
 }
